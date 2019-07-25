a1982f6b2e9a57f93d457be3f6fb3d3479e0584f
Kevin <kevinl@KL-2.local>
2019-07-25 21:07:22 +0800

    feat: This is a minor fix

---
+++ custom/+lsp.el
@@ -0,0 +1,14 @@
+;;; ~/.doom.d/custom/+lsp.el -*- lexical-binding: t; -*-
+
+;; lsp
+;; lsp-ui
+(with-eval-after-load 'lsp-mode
+  (setq lsp-ui-sideline-enable nil
+        lsp-ui-sideline-ignore-duplicate t
+        lsp-ui-doc-enable nil
+        lsp-ui-doc-include-singature nil
+        lsp-ui-doc-header nil)
+  (add-to-list 'lsp-file-watch-ignored "build"))
+
+;; lsp-formatting
+(add-hook 'before-save-hook (lambda () (when (eq major-mode 'python-mode) (lsp-format-buffer))))
---
+++ custom/+org.el
@@ -0,0 +1,68 @@
+;;; ~/.doom.d/custom/+org.el -*- lexical-binding: t; -*-
+
+;;
+;; Org-mode
+(after! org
+  (setq outline-blank-line nil
+        org-cycle-separator-lines 2
+        org-log-done 'time
+        org-directory "~/OneDrive/org"
+        org-agenda-files '("~/OneDrive/org" "~/org/brain")
+        org-latex-caption-above nil)
+  ;; Org-links to emails
+  (require 'org-notmuch))
+
+;; Hooks
+(add-hook 'org-mode-hook(lambda ()
+                          (hl-line-mode -1)))
+(add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images)
+(add-hook 'org-mode-hook #'doom|disable-line-numbers)
+
+;;
+;; Notes
+(def-package! org-noter
+  :after org
+  :config
+  (setq org-noter-always-create-frame nil
+        org-noter-auto-save-last-location t)
+  (map! :localleader
+        :map org-mode-map
+        (:prefix-map ("n" . "org-noter")
+          :desc "Open org-noter" :n "o" #'org-noter
+          :desc "Kill org-noter session" :n "k" #'org-noter-kill-session
+          :desc "Insert org-note" :n "i" #'org-noter-insert-note
+          :desc "Insert precise org-note" :n "p" #'org-noter-insert-precise-note
+          :desc "Sync current note" :n "." #'org-noter-sync-current-note
+          :desc "Sync next note" :n "]" #'org-noter-sync-next-note
+          :desc "Sync previous note" :n "[" #'org-noter-sync-prev-note)))
+
+;;
+;; Blog
+(def-package! ox-hugo
+  :defer t
+  :after ox)
+
+;;
+;; Jira
+(def-package! org-jira
+  :defer t
+  :config
+  ;; Fix access
+  (setq jiralib-url "https://jira.zenuity.com"
+        org-jira-users `("Niklas Carlsson" . ,(shell-command-to-string "printf %s \"$(pass show work/zenuity/login | sed -n 2p | awk '{print $2}')\""))
+        jiralib-token `("Cookie". ,(my/init-jira-cookie)))
+  ;; Customization jira query
+  (setq org-jira-custom-jqls
+        '(
+          (:jql " project = DUDE AND issuetype = Sub-task AND sprint in openSprints() AND sprint NOT IN futureSprints()"
+                :limit 50
+                :filename "dude-current-sprint-sub-tasks")
+          (:jql " project = DUDE AND assignee = currentuser() order by created DESC "
+                :limit 20
+                :filename "dude-niklas")
+          ))
+  ;; Customize the flow
+  (defconst org-jira-progress-issue-flow
+    '(("To Do" . "In Progress")
+      ("In Progress" . "Review")
+      ("Review" . "Done"))))
